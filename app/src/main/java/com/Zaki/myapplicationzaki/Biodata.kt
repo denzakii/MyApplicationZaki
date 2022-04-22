25957bca66570ee057304564efda96fdaab2d6ed
Hariskybagustio <1910031802060@sar.ac.id>
2022-04-22 23:53:57 +0700

    Initial commit

---
+++ app/src/main/java/com/Zaki/myapplicationzaki/Biodata.kt
@@ -0,0 +1,13 @@
+package com.Zaki.myapplicationzaki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Biodata : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_biodata)
+
+    }
+}
